c48ad9243ce00a2787e52bd7310dd4a1a005c939
Brian Klehr <brian67.bk@gmail.com>
2022-12-11 20:37:58 -0300

    Se agrega el proyecto

---
+++ public/js/servicios/carrito.js
@@ -0,0 +1,12 @@
+class CarritoService {
+    //URL_CARRITO = 'https://615d8b5212571a00172076ba.mockapi.io/carrito/'
+    URL_CARRITO = '/api/carrito/'
+
+    async guardarCarritoServicio(carrito) {
+        const carritoGuardado = await http.post(this.URL_CARRITO, carrito)
+        return carritoGuardado
+    }
+
+}
+
+const carritoService = new CarritoService()
